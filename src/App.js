e92431c18d4f478c592632225572bc108be92ecb
behruz171 <behruzzo662@gmail.com>
2023-02-08 18:44:54 +0500

    nimdir o'zgardi

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <h1>main</h1>
-        <p>nimadir</p>
-        <h2>obey</h2>
       </header>
     </div>
   );
